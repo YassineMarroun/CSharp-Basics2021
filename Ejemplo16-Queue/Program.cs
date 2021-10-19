12cae5812503a664c983085531250a688a67f329
YassineMarroun <el.yasin@hotmail.com>
2021-10-19 22:12:53 +0200

    Commit

---
+++ Ejemplo16-Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejemplo16_Queue
+{
+    /*
+     * Ejemplo de uso de Cola
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creo la cola
+            Queue<char> colaABC = new Queue<char>();
+            // Encolo elementos
+            colaABC.Enqueue('a');
+            colaABC.Enqueue('b');
+            colaABC.Enqueue('c');
+            colaABC.Enqueue('d');
+            colaABC.Enqueue('e');
+            colaABC.Enqueue('f');
+
+            // Uso un bucle para desencolar
+            while (colaABC.Count > 0)
+            {
+                Console.WriteLine(colaABC.Dequeue());
+            }
+        }
+    }
+}
---
+++ Ejemplo17-List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejemplo17_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
