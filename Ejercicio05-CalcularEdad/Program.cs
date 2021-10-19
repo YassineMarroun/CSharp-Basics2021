304dbd8097ee102f0424ecd8e3ddb0b79965c294
YassineMarroun <el.yasin@hotmail.com>
2021-10-19 23:06:29 +0200

    Commit

---
+++ Ejercicio05-CalcularEdad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio05_CalcularEdad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime hoy = DateTime.Today;
+            // Pregunto al usuario por su fecha de nacimiento
+            Console.WriteLine("Introduce el año que naciste:");
+            int anno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el número de mes que naciste:");
+            int mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introuce el número del día del mes en que naciste:");
+            int dia = int.Parse(Console.ReadLine());
+
+            int edad = 0;
+            edad = hoy.Year - anno;
+            if (mes > hoy.Month)
+            {
+                edad--;
+            }
+            if (mes == hoy.Month && dia > hoy.Day)
+            {
+                edad--;
+            }
+            Console.WriteLine($"Tienes {edad} años");
+
+        }
+    }
+}
