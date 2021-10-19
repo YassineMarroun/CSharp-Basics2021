c62de0946713302195e793c5d928ecbcf2b2af66
YassineMarroun <el.yasin@hotmail.com>
2021-10-19 22:00:23 +0200

    Commit

---
+++ Ejemplo15-Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejemplo15_Stack
+{
+    /*
+     * Ejemplo de uso de Stack
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creo la pila
+            Stack<char> pilaABC = new Stack<char>();
+            // Apilo elementos
+            pilaABC.Push('a');
+            pilaABC.Push('b');
+            pilaABC.Push('c');
+            pilaABC.Push('d');
+            pilaABC.Push('e');
+            pilaABC.Push('f');
+
+            // Uso un bucle para desapilar
+            while (pilaABC.Count > 0)
+            {
+                Console.WriteLine(pilaABC.Pop());
+            }
+        }
+    }
+}
