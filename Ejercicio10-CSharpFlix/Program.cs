8e514fab2ebacd3a70309c8521dc263a48f967d2
YassineMarroun <el.yasin@hotmail.com>
2021-10-21 15:19:10 +0200

    Commit

--- Ejercicio10-CSharpFlix/Program.cs
+++ Ejercicio10-CSharpFlix/Program.cs
@@ -25,9 +25,10 @@ namespace Ejercicio10_CSharpFlix
                     Console.WriteLine("----------------------------");
                     Console.WriteLine("|  (1) Listar películas    |");
                     Console.WriteLine("|  (2) Añadir película     |");
-                    Console.WriteLine("|  (3) Salir del programa  |");
+                    Console.WriteLine("|  (3) Eliminar película   |");
+                    Console.WriteLine("|  (4) Salir del programa  |");
                     Console.WriteLine("----------------------------");
-                    Console.WriteLine("Introduce una opción (0-2)");
+                    Console.WriteLine("Introduce una opción (1-4)");
                     opcion = int.Parse(Console.ReadLine());
 
                     switch (opcion)
@@ -51,6 +52,35 @@ namespace Ejercicio10_CSharpFlix
                             Console.WriteLine("Introduce la duración en minutos: ");
                             peliNueva.duracion = int.Parse(Console.ReadLine());
                             peliculas.Add(peliNueva);
+                            break;
+                        case 3:
+                            // Eliminar películas
+                            // Comprobamos que existen películas
+                            if (peliculas.Count > 0)
+                            {
+                                // Hay películas
+                                // Le pedimos el nombre de la película que desea eliminar
+                                Console.WriteLine("Introduce el título de la película a eliminar: ");
+                                string titulo = Console.ReadLine();
+                                // Recorremos la lista de películas
+                                for (int i = peliculas.Count-1; i >= 0; i--)
+                                {
+                                    if (peliculas[i].titulo.Contains(titulo))
+                                    {
+                                        // El título contiene el criterio de búsqueda
+                                        // Le preguntamos si desea borrarla
+                                        Console.WriteLine("¿Estás seguro de que deseas borrar la siguiente película (s/n)?");
+                                        Console.WriteLine("Titulo: {0}; Director: {1}; Duración: {2} minutos.",
+                                            peliculas[i].titulo, peliculas[i].director, peliculas[i].duracion);
+                                        char respuesta = char.Parse(Console.ReadLine().ToLower());
+                                        if (respuesta == 's')
+                                        {
+                                            // La borramos
+                                            peliculas.RemoveAt(i);
+                                        }
+                                    }
+                                }
+                            }
                             break;
                     }
 
