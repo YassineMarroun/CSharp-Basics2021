35610dffff3b9c3fa8a4ab3b1ac788f2578a06fa
YassineMarroun <el.yasin@hotmail.com>
2021-10-24 23:05:13 +0200

    Commit

---
+++ Ejemplo21-ArchivoTexto/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ejemplo21_ArchivoTexto
+{
+    /*
+     * Ejemplo con archivo de texto plano
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creo un string con la ruta del archivo
+            string rutaArchivo = @"C:\Users\yasma2\OneDrive - UNED\.ProyectosGITHUB\CSharp - Basics2021\nombres.txt";
+            
+            // Declaro las variables que usaré
+            string cadenaNombres = null;
+            string[] arrayNombres;
+            List<string> listaNombres;
+            
+            try
+            {
+                // Leemos el archivo
+                StreamReader sr = new StreamReader(rutaArchivo);
+                cadenaNombres = sr.ReadLine();
+                sr.Close();
+            } catch (FileNotFoundException)
+            {
+                Console.WriteLine("El archivo no existe");
+            }
+            
+
+            // Muestro la información recuperada
+            Console.WriteLine(cadenaNombres);
+
+            if (cadenaNombres != null)
+            {
+                // El archivo existe y contiene nombres
+                // Pasamos la cadena del archivo a un array de string
+                // indicando que el separador es la coma (,)
+                arrayNombres = cadenaNombres.Split(',');
+
+                //Creamos una lista a partir del array
+                listaNombres = new List<string>(arrayNombres);
+            } else
+            {
+                // No existe el archivo o no tiene nombres
+                listaNombres = new List<string>();
+            }
+
+            char opcion;
+            do
+            {
+                Console.WriteLine("(L)istar nombres");
+                Console.WriteLine("(A)ñadir nombre");
+                Console.WriteLine("(S)alir del programa");
+                Console.WriteLine("Introduce una opción [L, A, S]:");
+                opcion = char.Parse(Console.ReadLine().ToLower());
+
+                switch(opcion)
+                {
+                    case 'l':
+                        // Listar
+                        for (int i = 0; i < listaNombres.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {listaNombres[i]}");
+                        }
+                        break;
+                    case 'a':
+                        // Añadir
+                        Console.WriteLine("Introduce un nuevo nombre:");
+                        listaNombres.Add(Console.ReadLine());
+                        break;
+                    case 's':
+                        // Salir del programa
+                        // Convertimos el List en un array
+                        arrayNombres = listaNombres.ToArray();
+                        // Unimos los nombres del array en una cadena utilizando (,) como separador
+                        cadenaNombres = String.Join(',', arrayNombres);
+                        // Guardamos el archivo
+                        StreamWriter sw = new StreamWriter(rutaArchivo);
+                        sw.WriteLine(cadenaNombres);
+                        sw.Close();
+                        Console.WriteLine("Saliendo del programa. Hasta la vista.");
+                        break;
+                    default:
+                        Console.WriteLine("Opción incorrecta");
+                        break;
+                }
+            } while (opcion != 's');
+        }
+    }
+}
--- Ejercicio10-CSharpFlix/Program.cs
+++ Ejercicio10-CSharpFlix/Program.cs
@@ -16,77 +16,110 @@ namespace Ejercicio10_CSharpFlix
             // Creamos una lista de películas
             List<Pelicula> peliculas = new List<Pelicula>();
             do{
-                    Console.WriteLine("----------------------------");
+                PintaMenu();   
+                Console.WriteLine("Introduce una opción (0-3)");
+                opcion = int.Parse(Console.ReadLine());
+                
+                switch (opcion)
+                {
+                    case 1:
+                        // Listar películas
+                        ListarPelicula(peliculas);
+                        break;
+                    case 2:
+                        // Añadir película
+                        AnnadirPelicula(peliculas);
+                        break;
+                    case 3:
+                        // Eliminar películas
+                        EliminarPelicula(peliculas);
+                        break;
+                }
+
+            } while (opcion != 0);
+            Console.WriteLine($"Adiós {usuario}, ¡Hasta la vista!");
+        }
+
+
+        private static void PintaMenu()
+        {
+             Console.WriteLine("----------------------------");
                     Console.WriteLine("|        CSharpFlix        |");
                     Console.WriteLine("----------------------------");
                     Console.WriteLine("|  (1) Listar películas    |");
                     Console.WriteLine("|  (2) Añadir película     |");
                     Console.WriteLine("|  (3) Eliminar película   |");
-                    Console.WriteLine("|  (4) Salir del programa  |");
+                    Console.WriteLine("|  (0) Salir del programa  |");
                     Console.WriteLine("----------------------------");
-                    Console.WriteLine("Introduce una opción (1-4)");
-                    opcion = int.Parse(Console.ReadLine());
+        }
 
-                    switch (opcion)
+
+        private static void ListarPelicula(List<Pelicula> peliculas)
+        {
+            // Comprobamos que existan películas
+            if (peliculas.Count > 0)
+            {
+                // Hay películas y las mostramos
+                Console.WriteLine("Listado de películas");
+                Console.WriteLine("--------------------");
+                foreach(Pelicula peli in peliculas)
+                {
+                    Console.WriteLine(peli);
+                }
+            } else
+            {
+                // La lista de películas está vacía
+                Console.WriteLine("No existen películas en el sistema.");
+            }
+        }
+
+
+        private static void AnnadirPelicula(List<Pelicula> peliculas)
+        {
+            // Creamos una película y la añadimos a la lista
+            Pelicula peliNueva = new Pelicula();
+            Console.WriteLine("Introduce el título de la película: ");
+            peliNueva.Titulo = Console.ReadLine();
+            Console.WriteLine("Introduce la duración en minutos: ");
+            peliNueva.Duracion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce la descripción: ");
+            peliNueva.Descripcion = Console.ReadLine();
+            peliNueva.Fecha = DateTime.Today;                           
+            Console.WriteLine("Introduce el nombre del director/a: ");
+            peliNueva.Director = Console.ReadLine();
+            Console.WriteLine("Introduce el género de la película: ");
+            peliNueva.Genero = Console.ReadLine();
+            peliculas.Add(peliNueva);
+        }
+
+
+        private static void EliminarPelicula(List<Pelicula> peliculas)
+        {
+            // Comprobamos que existen películas
+            if (peliculas.Count > 0)
+            {
+                // Hay películas
+                // Le pedimos el nombre de la película que desea eliminar
+                Console.WriteLine("Introduce el título de la película a eliminar: ");
+                string titulo = Console.ReadLine();
+                // Recorremos la lista de películas
+                for (int i = peliculas.Count-1; i >= 0; i--)
+                {
+                    if (peliculas[i].Titulo.Contains(titulo))
                     {
-                        case 1:
-                            // Listamos las películas
-                            Console.WriteLine("Listado de películas");
-                            Console.WriteLine("--------------------");
-                            foreach(Pelicula peli in peliculas)
-                            {
-                                Console.WriteLine(peli);
-                            }
-                            break;
-                        case 2:
-                            // Creamos una película y la añadimos a la lista
-                            Pelicula peliNueva = new Pelicula();
-                            Console.WriteLine("Introduce el título de la película: ");
-                            peliNueva.Titulo = Console.ReadLine();
-                            Console.WriteLine("Introduce la duración en minutos: ");
-                            peliNueva.Duracion = int.Parse(Console.ReadLine());
-                            Console.WriteLine("Introduce la descripción: ");
-                            peliNueva.Descripcion = Console.ReadLine();
-                            peliNueva.Fecha = DateTime.Today;                           
-                            Console.WriteLine("Introduce el nombre del director/a: ");
-                            peliNueva.Director = Console.ReadLine();
-                            Console.WriteLine("Introduce el género de la película: ");
-                            peliNueva.Genero = Console.ReadLine();
-
-                            peliculas.Add(peliNueva);
-                            break;
-                        case 3:
-                            // Eliminar películas
-                            // Comprobamos que existen películas
-                            if (peliculas.Count > 0)
-                            {
-                                // Hay películas
-                                // Le pedimos el nombre de la película que desea eliminar
-                                Console.WriteLine("Introduce el título de la película a eliminar: ");
-                                string titulo = Console.ReadLine();
-                                // Recorremos la lista de películas
-                                for (int i = peliculas.Count-1; i >= 0; i--)
-                                {
-                                    if (peliculas[i].Titulo.Contains(titulo))
-                                    {
-                                        // El título contiene el criterio de búsqueda
-                                        // Le preguntamos si desea borrarla
-                                        Console.WriteLine("¿Estás seguro de que deseas borrar la siguiente película (s/n)?");
-                                        Console.WriteLine(peliculas[i]);
-                                        char respuesta = char.Parse(Console.ReadLine().ToLower());
-                                        if (respuesta == 's')
-                                        {
-                                            // La borramos
-                                            peliculas.RemoveAt(i);
-                                        }
-                                    }
-                                }
-                            }
-                            break;
+                        // El título contiene el criterio de búsqueda
+                        // Le preguntamos si desea borrarla
+                        Console.WriteLine("¿Estás seguro de que deseas borrar la siguiente película (s/n)?");
+                        Console.WriteLine(peliculas[i]);
+                        char respuesta = char.Parse(Console.ReadLine().ToLower());
+                        if (respuesta == 's')
+                        {
+                            // La borramos
+                            peliculas.RemoveAt(i);
+                        }
                     }
-
-            } while (opcion != 4);
-            Console.WriteLine($"Adiós {usuario}, ¡Hasta la vista!");
+                }
+            }
         }
     }
 }
